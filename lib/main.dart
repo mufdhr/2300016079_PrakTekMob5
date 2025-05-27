44438330de74f0bd0f66de18ed543cef8fde9a48
mufdhr <149158097+mufdhr@users.noreply.github.com>
2025-05-27 21:18:52 +0700

    2300016079_PrakTekMob5

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,18 +16,51 @@ class MyApp extends StatelessWidget {
 }
 
 class ECommerceScreen extends StatelessWidget {
-  const ECommerceScreen({Key? key}) : super(key: key);
+  const ECommerceScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // Tambahkan AppBar
       appBar: AppBar(
-        title: const Text('Amajon'),
+        title: const Text('Amajon Store'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.notifications),
+            onPressed: () {},
+            padding: const EdgeInsets.all(12.0),
+          ),
+        ],
       ),
+    
+     
+
+      // Tambahkan BottomNavigationBar
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Beranda',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Akun',
+          ),
+        ],
+      ),
+
+      // Tambahkan FloatingActionButton
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {}, 
+        child: const Icon(Icons.shopping_cart),
+      ),
+
       body: SafeArea(
+        child: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
+          children: [
             // Banner Promo
             Container(
               margin: const EdgeInsets.all(16.0),
@@ -46,15 +79,49 @@ class ECommerceScreen extends StatelessWidget {
                 ),
               ),
             ),
+            // Baris diskon dan gratis ongkir
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 12.0),
+                color: Colors.grey[100],
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      children: [
+                        const Icon(Icons.discount, color: Colors.red),
+                        const SizedBox(width: 4.0),
+                        Text('Diskon', style: TextStyle(color: Colors.grey[800])),
+                      ],
+                    ),
+                    Text('Gratis Ongkir Seluruh Indonesia', 
+                         style: TextStyle(color: Colors.grey[800], fontWeight: FontWeight.w900)),
+                    Row(
+                      children: [
+                        const Icon(Icons.local_shipping, color: Colors.blue),
+                        const SizedBox(width: 4.0),
+                        Text('Kirim Cepat', style: TextStyle(color: Colors.grey[800])),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+                  
 
             // Produk 1
             Container(
-              margin:
-                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              padding: const EdgeInsets.all(16.0),
+              margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              padding: const EdgeInsets.only(left: 24.0, top: 16.0, right: 16.0, bottom: 12.0),
               decoration: BoxDecoration(
                 border: Border.all(color: Colors.grey.shade300),
                 borderRadius: BorderRadius.circular(8.0),
+                boxShadow: [
+                  BoxShadow(
+                  // ignore: deprecated_member_use
+                  color:  Colors.pink.withOpacity(0.3),
+                  blurRadius: 2,
+                  offset: const Offset(0, 1),
+                ),
+              ],
               ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -66,7 +133,22 @@ class ECommerceScreen extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
+                  const SizedBox(height: 4.0),
+                  const Row(
+                    children: [
+                      Icon(Icons.star, color: Colors.orange, size: 16),
+                      SizedBox(width: 4),
+                      Text('4.8 (220)'),
+                    ],
+                  ),
                   const SizedBox(height: 8.0),
+                 Row(
+                   children: [
+                      Icon(Icons.local_shipping, color: Colors.grey.shade700, size: 16),
+                      const SizedBox(width: 4.0),
+                      const Text('Estimasi tiba 2-3 hari'),
+                   ],
+                 ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
@@ -79,7 +161,7 @@ class ECommerceScreen extends StatelessWidget {
                       ),
                       Container(
                         padding: const EdgeInsets.symmetric(
-                            horizontal: 12.0, vertical: 6.0),
+                          horizontal: 16.0, vertical: 8.0),
                         decoration: BoxDecoration(
                           color: Colors.blue,
                           borderRadius: BorderRadius.circular(4.0),
@@ -104,11 +186,18 @@ class ECommerceScreen extends StatelessWidget {
             Container(
               margin:
                   const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              padding: const EdgeInsets.all(16.0),
+             padding: const EdgeInsets.only(left: 24.0, top: 16.0, right: 16.0, bottom: 12.0),
               decoration: BoxDecoration(
                 border: Border.all(color: Colors.grey.shade300),
                 borderRadius: BorderRadius.circular(8.0),
-              ),
+                boxShadow: [
+                 BoxShadow(
+                  color: Colors.green.shade50,
+                  blurRadius: 2,
+                  offset: const Offset(0, 1),
+                ),
+              ],
+            ), 
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
@@ -119,7 +208,22 @@ class ECommerceScreen extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
+                  const SizedBox(height: 4.0),
+                  const Row(
+                    children: [
+                      Icon(Icons.star, color: Colors.orange, size: 16),
+                      SizedBox(width: 4),
+                      Text('4.4 (100)'),
+                    ],
+                  ),
                   const SizedBox(height: 8.0),
+                  Row(
+                   children: [
+                      Icon(Icons.local_shipping, color: Colors.grey.shade700, size: 16),
+                      const SizedBox(width: 4.0),
+                      const Text('Estimasi tiba 2-3 hari'),
+                   ],
+                 ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
@@ -132,7 +236,7 @@ class ECommerceScreen extends StatelessWidget {
                       ),
                       Container(
                         padding: const EdgeInsets.symmetric(
-                            horizontal: 12.0, vertical: 6.0),
+                          horizontal: 16.0, vertical: 8.0),
                         decoration: BoxDecoration(
                           color: Colors.blue,
                           borderRadius: BorderRadius.circular(4.0),
@@ -153,39 +257,41 @@ class ECommerceScreen extends StatelessWidget {
               ),
             ),
 
+
             // Kategori
-            Container(
-              margin: const EdgeInsets.all(16.0),
-              child: Row(
+             Container(
+              margin:  const EdgeInsets.all(16.0),
+              child:  const Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
                   Column(
-                    children: const <Widget>[
-                      Icon(Icons.shopping_bag, color: Colors.blue),
-                      SizedBox(height: 8.0),
-                      Text('Pakaian'),
+                    children:  <Widget>[
+                       Icon(Icons.shopping_bag, color: Colors.blue),
+                       SizedBox(height: 8.0),
+                       Text('Pakaian'),
                     ],
                   ),
                   Column(
-                    children: const <Widget>[
-                      Icon(Icons.watch, color: Colors.blue),
-                      SizedBox(height: 8.0),
-                      Text('Aksesoris')
+                    children:  <Widget>[
+                       Icon(Icons.watch, color: Colors.blue),
+                       SizedBox(height: 8.0),
+                       Text('Aksesoris')
                     ],
                   ),
                   Column(
-                    children: const <Widget>[
-                      Icon(Icons.devices, color: Colors.blue),
-                      SizedBox(height: 8.0),
-                      Text('Elektronik'),
+                    children:  <Widget>[
+                       Icon(Icons.devices, color: Colors.blue),
+                       SizedBox(height: 8.0),
+                       Text('Elektronik'),
                     ],
-                  )
-                ],
+                  ),  
+                ], 
               ),
             ),
-          ],
-        ),
-      ),
-    );
+            ],   
+                ),
+              ),
+            ),
+            );        
+    }
   }
-}
